06d727f7fa58697e78a55000f1d3cb2ab5e7a4bd
syed omer farooq <syedomer.farooq@zemosolabs.com>
2021-12-17 11:06:14 +0530

    final commit

---
+++ Factory-method-pattern/src/com/example/demo/CommercialPlan.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class CommercialPlan extends Plan {
+    //@override
+    public void getRate() {
+        rate = 7.50;
+    }
+}
---
+++ Factory-method-pattern/src/com/example/demo/InstitutionalPlan.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class InstitutionalPlan extends Plan{
+    //@override
+    public void getRate(){
+        rate=5.50;
+    }
+}
